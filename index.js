404cf01b97d5e8568efa5956a9277f3f5e1687d7
grisha <grisha_petrosyan@epam.com>
2019-02-27 19:38:58 +0400

    adding sockets

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   console.log('connected');
+
+  socket.on('offer', function (data) {
+    socket.emit('offer', data);
+  });
+
+  socket.on('answer', function (data) {
+    socket.emit('answer', data);
+  });
+
+  socket.on('candidate', function (data) {
+    socket.emit('candidate', data);
+  });
 });
